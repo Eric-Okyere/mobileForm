84f50dd8c47b6c23bd1807a130ee5ef0fc9a66ff
Eric Okyere <ericokyere018@gmail.com>
2023-02-26 08:22:53 +0000

    mobileForm

--- App.js
+++ App.js
@@ -1,20 +1,165 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, { Component } from 'react'
+import LoginPage from './LoginPage';
+
+// export default class App extends Component {
+//   constructor(props){
+//     super(props)
+//     this.state={
+//       name:"",
+//       email:"",
+//       password:""
+//     }
+//   }
+  
+//   render() {
+//     return (
+  //     <View style={styles.container}>
+  //     <View style={styles.form}>
+  //     <Text style={styles.header}>Feel free to register with us</Text>
+  //    <Text style={styles.Text}>Name</Text>
+  //       <TextInput style={styles.input}
+  //       autoCapitalize
+  //       value={this.state.name}
+  //       onChange={(name)=>{
+  //         this.setState({name})
+  //       }}
+  //       autoCorrect={true}
+  //       placeholder= 'Enter name' />
+
+  //    <Text style={styles.Text}>Email</Text>
+  //       <TextInput style={styles.input}
+  //       autoCapitalize="none"
+  //       placeholder='Enter email' 
+  //       value={this.state.email}
+  //        onChange={(email)=>{
+  //         this.setState({email})
+  //       }}
+  //       />
+
+  //    <Text style={styles.Text}>Password</Text>
+  //       <TextInput style={styles.input}
+  //       autoCapitalize="none"
+  //       placeholder='Enter password'
+  //       secureTextEntry={true}
+  //     value={this.state.password}
+  //      onChange={(password)=>{
+  //         this.setState({password})
+  //       }}
+  //       />
+  //   <TouchableOpacity style={styles.button}>
+  //     <Text >Signup</Text>
+  //   </TouchableOpacity>
+  //       </View>
+      
+  
+  //   </View>
+
+
+
+
+//     )
+//   }
+// }
+
+
+// export default class App extends Component {
+
+//   constructor(props){
+//         super(props)
+//         this.state={
+//           name:["Eric", "Okyere","Joe","Mary","Eric", "Okyere","Joe","Mary","Eric", "Okyere","Joe",, "Okyere","Joe","Mary","Eric", "Okyere","Joe","Mary","Eric", "Okyere","Joe","Mary","Eric", "Okyere","Joe","Mary","Eric", "Okyere","Joe","Mary","Eric", "Okyere","Joe","Mary","Eric", "Okyere","Joe","Mary","Eric", "Okyere","Joe","Mary","Eric", "Okyere","Joe","Mary","Eric", "Okyere","Joe","Mary","Eric", "Okyere","Joe","Mary","Eric", "Okyere","Joe","Mary","Eric", "Okyere","Joe","Mary"],
+          
+//         }
+//       }
+
+
+//   render() {
+//     return (
+//      <View style={Mystyles.container}>
+//       <FlatList 
+//       data={this.state.name}
+//       renderItem={({item})=>{
+//       return<Text>{item}</Text>
+    
+//       }}
+//       keyExtractor={(item, index)=>index}
+//       />
+
+      
+      
+//      </View>
+//     )
+//   }
+// }
+
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={Mystyles.container} >
+        <LoginPage />
+      
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+
+
+const Mystyles = StyleSheet.create({
+ container: {
+   top:85,
+    
+ 
+   
+
+    
   },
+//   input:{
+//    backgroundColor:"white",
+//     width:345,
+//     borderColor:"white",
+//     borderWidth:1,
+//     borderRadius:20,
+//    paddingHorizontal:5,
+//    marginBottom:10,
+//    height:25
+//   },
+//   form:{
+//     backgroundColor:"black",
+//     padding:5,
+//     borderRadius:"10",
+   
+  
+//   },
+//   Text:{
+//     color:"white"
+//   },
+//   button:{
+//     backgroundColor:"white",
+//     color:"black",
+   
+//     alignItems:"center",
+//     alignSelf:"center",
+//     width:150,
+//    marginVertical:20,
+//     borderRadius:10,
+//    height:20,
+// display:"flex",
+
+//   },
+//   header:{
+//     alignSelf:"center",
+//     color:"white",
+//     fontWeight:"bold",
+//     fontSize:"lager"
+//   }
+
 });
---
+++ LoginPage.jsx
@@ -0,0 +1,106 @@
+import React, { Component } from 'react'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView } from 'react-native'
+
+export default class LoginPage extends Component {
+  render() {
+    return (
+      <View style={Mystyle.main}>
+     <ScrollView style={Mystyle.container} showsVerticalScrollIndicator={false}>
+       <View style={Mystyle.headerCont}>
+         <Text style={Mystyle.header}>Login</Text>
+         </View>
+
+       <View>
+
+      <TextInput style={Mystyle.input}
+      autoCapitalize
+      autoCorrect={true}
+      placeholder='Username' />
+      <TextInput style={Mystyle.input}
+      secureTextEntry={true}
+      placeholder='Password' />
+     
+      
+      
+      <Text style={Mystyle.FogPass}>Forgot password?</Text>
+       </View>
+
+      <View>
+      <TouchableOpacity style={Mystyle.butCont}>
+        <Text style={Mystyle.butText}>Log in</Text>
+      </TouchableOpacity>
+      </View>
+
+      <View style={Mystyle.noAccCont}>
+      <Text style={Mystyle.Dont}>Don't have account? </Text>
+      <Text style={Mystyle.signUp}>Sign up</Text>
+      </View>
+     </ScrollView>
+     </View>
+    )
+  }
+}
+
+const Mystyle = StyleSheet.create({
+  main:{
+    borderWidth:1,
+    borderColor:"#989ea3",
+    margin:10,
+    borderRadius:20
+  },
+  container:{
+    marginHorizontal:50,
+  
+    
+  },
+  header:{
+    fontSize:50,
+    color:"#8d11ba",
+   
+  },
+
+  headerCont:{
+    marginBottom:50,
+  },
+
+  input:{
+    borderBottomWidth:2,
+    borderBottomColor:"#8d11ba",
+    height:50,
+    fontSize:20,
+    marginTop:20
+  },
+  FogPass:{
+    alignSelf:"flex-end",
+    marginVertical:15,
+    color:"#4e95d4"
+  },
+  butCont:{
+    height:50,
+    backgroundColor:"#8d11ba",
+    justifyContent:"center",
+    alignItems:"center",
+    borderRadius:10,
+    marginVertical:30
+  },
+  butText:{
+    color:"white",
+    fontSize:25
+  },
+  noAccCont:{
+    flexDirection:"row",
+   justifyContent:"center",
+   
+  },
+  Dont:{
+    marginRight:10,
+    fontSize:16,
+    
+  },
+  signUp:{
+    fontSize:16,
+    color:"#4e95d4",
+    marginBottom:100
+  }
+
+})
